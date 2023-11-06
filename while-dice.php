f0ad6516b5b80129ac86cc3973f418b524877365
gamblerLt <rimas.spakauskas@outlook.com>
2023-11-06 20:42:25 +0200

    11 line </p> added

--- while-dice.php
+++ while-dice.php
@@ -8,6 +8,6 @@ if($dice == 6) {
     echo '<p>Your dice is ' . $dice . '. ' . 'You win!</p>';
 
 } else {
-    echo '<p> Your dice is '. $dice . '. '. 'Try again!';
+    echo '<p> Your dice is '. $dice . '. '. 'Try again!</p>';
     } 
 }
