5e9cd3df00bd5c8fae1cd40de7639ff96435c608
risp-dev <119005269+risp-dev@users.noreply.github.com>
2023-11-06 15:19:28 +0200

    Code Beauty

--- dice.php
+++ dice.php
@@ -6,8 +6,7 @@ $dice = rand(1, 6);
 
 if ($dice == 6) {
 echo 'Jackpot!' . ' ' . "You've thrown a" . ' ' . $dice . '!';  
-}
-else{
+} else {
     header("Refresh:3");
     echo 'Your number is'. ' ' . $dice . '!' . ' ' . 'Your browser will refresh after 3 sec. or you  may do it manually!'; 
 }
