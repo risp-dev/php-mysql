235cc68e5713813f06dc8894d82ec7eeb9243309
gamblerLt <rimas.spakauskas@outlook.com>
2023-11-06 19:38:47 +0200

    Dummy multiplication

---
+++ multiplication.php
@@ -0,0 +1,21 @@
+<?php
+
+$one = 1;
+$two = 2;
+$three = 3;
+$four = 4;
+$five = 5;
+$six = 6;
+$seven = 7;
+$eight = 8;
+$nine = 9;
+
+echo '<p>4*1 =' . ' ' . $four * $one . ' ' . '</p>';
+echo '<p>4*2 =' . ' ' . $four * $two . ' ' . '</p>';
+echo '<p>4*3 =' . ' ' . $four * $three . ' ' . '</p>';
+echo '<p>4*4 =' . ' ' . $four * $four . ' ' . '</p>';
+echo '<p>4*5 =' . ' ' . $four * $five . ' ' . '</p>';
+echo '<p>4*6 =' . ' ' . $four * $six . ' ' . '</p>';
+echo '<p>4*7 =' . ' ' . $four * $seven . ' ' . '</p>';
+echo '<p>4*8 =' . ' ' . $four * $eight . ' ' . '</p>';
+echo '<p>4*9 =' . ' ' . $four * $nine . ' ' . '</p>';
