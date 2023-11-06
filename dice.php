147c672f442db41ea2003844f7a844d19b541028
risp-dev <119005269+risp-dev@users.noreply.github.com>
2023-11-06 13:58:21 +0200

    Fixed spelling

--- dice.php
+++ dice.php
@@ -9,7 +9,7 @@ echo 'Jackpot!' . ' ' . "You've thrown a" . ' ' . $dice . '!';
 }
 else{
     header("Refresh:3");
-    echo 'Your number is'. ' ' . $dice . '!' . ' ' . 'Your browser will refresh after 3 sec. or you  may do it manualy!'; 
+    echo 'Your number is'. ' ' . $dice . '!' . ' ' . 'Your browser will refresh after 3 sec. or you  may do it manually!'; 
 }
 
 echo '<br><br><a href="./dice.php">Refresh current page<a/>';
