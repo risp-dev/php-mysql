30804e7525d3bb74703b623913c435f76c98290e
risp-dev <119005269+risp-dev@users.noreply.github.com>
2023-11-10 11:35:39 +0200

    Affected rows

--- default/public/update.php
+++ default/public/update.php
@@ -6,12 +6,12 @@ $pdo = new PDO("mysql:host=localhost;dbname=november;charset=utf8mb4", 'rispdev'
 $output = 'Database connection established.';
 
    //$sql = 'UPDATE test SET `textdate` = "2002-04-09", `dumbtext` = "Pakeistas tekstas 3 eil" WHERE id = 3';
-   //$sql = 'UPDATE test SET `dumbtext` = "Pakeistas 5 tekstas" WHERE id = 5';
-   $sql = 'DELETE FROM test WHERE id = 10';
+   $sql = 'UPDATE test SET `dumbtext` = "Pakeistas 5 tekstas" WHERE id = 5';
+  //$sql = 'DELETE FROM test WHERE id = 10';
     
         
-    $pdo->exec($sql);
-    $output="Table updated.";
+    $affectedRows = $pdo->exec($sql);
+    $output="Table updated. Affected:  " . $affectedRows . ' row(s)!';
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {
