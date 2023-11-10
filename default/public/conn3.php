793ac9d3cb54420c914ceed0615de7e81315d27b
risp-dev <119005269+risp-dev@users.noreply.github.com>
2023-11-10 10:15:53 +0200

    pdo mod

--- default/public/conn3.php
+++ default/public/conn3.php
@@ -1,26 +1,22 @@
 <?php
 
-$servername = "localhost";
-$dbusername = "rispdev";
-$dbpassword = "69*kQuyt/Kly5";
-$dbname = "november";
+//$servername = "localhost";
+//$dbusername = "rispdev";
+//$dbpassword = "69*kQuyt/Kly5";
+//$dbname = "november";
 
-//$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
 try {
-$pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $dbusername, $dbpassword);
-//$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+$pdo = new PDO("mysql:host=localhost;dbname=november;charset=utf8mb4", 'rispdev', '69*kQuyt/Kly5');
 
 $output = 'Database connection established.';
 
- // Set the PDO error mode to exception
+ // Set the PDO error mode to exception catching thrown during subsequent database operations
  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo "Prisijungiau";
 } catch(PDOException $e) {
-    // If there's an error, catch it and print the error message
-    //echo "Connection failed: " . $e->getMessage();
+    // Catching connection errors
     $output = 'Unable to connect to the database server ' . $e->getMessage();
 }
 
 include __DIR__ .'/../templates/connection.html.php';
-//C:\xampp\htdocs\ninja\php-mysql\default\templates\connection.html.php
