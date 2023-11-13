d02fcb78d9c56e1521bdac5ba500c21b5d50847d
risp-dev <119005269+risp-dev@users.noreply.github.com>
2023-11-13 15:18:11 +0200

    Styling + form

---
+++ default/templates/addjoke.html.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Do</title>
+</head>
+<body>
+    <form action="addjoke.php" method="post">
+        <label for="dumbtext">Type your joke</label>
+        <textarea id="dumbtext" name="dumbtext" rows="3" cols="40"></textarea>
+        <input type="submit" name="submit" value="Add">
+</form>
+    
+</body>
+</html>
--- default/templates/layout.html.php
+++ default/templates/layout.html.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    
+    
     <title><?=$title; ?></title>
 </head>
 <body>
@@ -12,7 +15,8 @@
     <nav>
         <ul>
             <li><a href="./index.php">Home</a></li>
-            <li><a href="./select.php">Juokeliai<a/></li>
+            <li><a href="./select.php">Juokeliai</a></li>
+            <li><a href="./addjoke.php">Add a joke</a></li>
         </ul>
 </nav>
 <main>
