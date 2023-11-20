dde3bdc7eba449a9d018e16fe704cabfd693e555
risp-dev <119005269+risp-dev@users.noreply.github.com>
2023-11-20 16:06:03 +0200

    Joke list works

--- project23/public/index.php
+++ project23/public/index.php
@@ -1,5 +1,12 @@
 <?php
 
-
 include __DIR__ . '/../dbconn/conn.php';
+
+$sql = 'SELECT `joketext` FROM `joke`';
+$result = $pdo->query($sql);
+
+while ($row = $result->fetch()) {
+    $jokes[] = $row['joketext'];
+}
+include __DIR__ . '/../templates/jokelist.html.php';
 include __DIR__ . '/../templates/layout.html.php';
---
+++ project23/templates/jokelist.html.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List of jokes</title>
+</head>
+<body>
+    <?php if (isset($error)): ?>
+        <p>
+<?=$error; ?>
+    </p>
+    <?php else: ?>
+        <?php foreach ($jokes as $joke): ?>
+            <blocquote>
+                <p>
+            <?= htmlspecialchars($joke, ENT_QUOTES, 'UTF-8') ?>
+        </p>
+        </blockquote>
+        <?php endforeach; ?>
+        <?php endif; ?>
+</body>
+</html>
