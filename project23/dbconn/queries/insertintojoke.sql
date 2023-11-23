24b0c32e0433f0d931ecdc3c494c37b40c0c519d
Rimas <rimas.spakauskas@outlook.com>
2023-11-23 11:52:34 +0200

    Insert into joke

---
+++ project23/dbconn/queries/insertintojoke.sql
@@ -0,0 +1,12 @@
+INSERT INTO  joke (`joketext`, `jokedate`)
+VALUES
+('Raitelis be galvos šiaip nėra baisus, baisu, kai jis joja eskadrono priešakyje.', '2021-02-03'),
+('Kuo gilesnė bedugnė, į kurią krinti, tuo daugiau šansų išmokti skraidyti.', '2023-05-02'),
+('UNIX pasilik sau, DOS pasidalyk su draugu, "Windows" atiduok priešui.', '2021-05-09'),
+('Blogam boksininkui dantų pastos nereikia.', '2023-09-07'),
+('Labas, nuvažiavau laimingai, buciuoju. Tavo stogas!!!', '2020-12-25'),
+('Jei visi pianino klavišai juodi - pakelk dangtį.', '2019-11-22'),
+('Kiekviename iš mūsų miega genijus. Ir kiekvieną dieną vis giliau...', '1999-05-28'),
+('Net kai tave suvalgė, vis dėl to, tu, turi dvi išeitis.', '1991-08-20'),
+('Teleskopu į saulę galima pažiūrėti tik 2 kartus. Pirmą kartą kaire akim, antrą kartą dešine.', '1994-07-12'),
+('Kaip vadinasi žmogus be kairės akies, kairės ausies, kairės rankos ir kairės kojos? ALL RIGHT!', '1998-12-08')
