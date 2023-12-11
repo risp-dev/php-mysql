54dc88057764636a984363a62928bf9ef8286438
gamblerLt <rimas.spakauskas@outlook.com>
2023-12-11 21:09:38 +0200

    deletejoke amended

--- project23/public/deletejoke.php
+++ project23/public/deletejoke.php
@@ -1,21 +1,23 @@
 <?php
-
+try {
 include __DIR__ . '/../dbconn/conn.php';
+include __DIR__ . '/../dbconn/dbfunctions.php';
 
-$sql = 'DELETE FROM `joke` WHERE `id` = :id';
+//$sql = 'DELETE FROM `joke` WHERE `id` = :id';
+deleteJoke($pdo, $_POST['id']);
 
-$stmt = $pdo->prepare($sql);
+//$stmt = $pdo->prepare($sql);
 
-$stmt->bindValue(':id', $_POST['id']);
-$stmt->execute();
+//$stmt->bindValue(':id', $_POST['id']);
+//$stmt->execute();
 
-header('location: jokes.php');
-    
-    
-    
-    
-    
-    ob_start(); 
-    include __DIR__ .'/../templates/jokelist.html.php';
-    $output = ob_get_clean();
+header('Location: jokes.php');
+}catch(PDOException $e) {  
+    $title = 'Klaida';
+
+    $output = 'Database error ' . $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine();
+   // ob_start(); 
+   // include __DIR__ .'/../templates/jokelist.html.php';
+   // $output = ob_get_clean();
+}
     include __DIR__ . '/../templates/layout.html.php';
