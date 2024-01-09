f0f985baf49cd06f5d47cf6546a46c38dcee7654
Rimas <rimas.spakauskas@outlook.com>
2024-01-09 11:39:31 +0200

    title shortened

--- project23/templates/index.html.php
+++ project23/templates/index.html.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php $title ?></title>
+    <title><?=$title ?></title>
 </head>
 <body>
 <?php if (isset($error)): ?>
