312f008ad0a97d722c7f578c5925a99985b3a0bd
Rimas <rimas.spakauskas@outlook.com>
2024-01-18 15:09:47 +0200

    controllers added to index

--- project23/public/index.php
+++ project23/public/index.php
@@ -1,7 +1,8 @@
 <?php
 
-function loadTemplate($templateFileName, $variables) {
+function loadTemplate($templateFileName, $variables = []) {
     extract($variables);
+    
     ob_start();
     include __DIR__ .'/../templates/' . $templateFileName;
     return ob_get_clean();
@@ -11,53 +12,35 @@ try {
 include __DIR__ . '/../dbconn/conn.php';
 include __DIR__ . '/../classes/DatabaseTable.php';
 include __DIR__ . '/../controllers/JokeController.php';
+include __DIR__ . '/../controllers/AuthorController.php';
 
 $jokesTable = new DatabaseTable($pdo, 'joke', 'id');
 $authorsTable = new DatabaseTable($pdo, 'author', 'id');
 
-$jokeController = new JokeController($jokesTable, $authorsTable);
+$action = $_GET['actioon'] ?? 'home';
+$controllerName = $_GET['controller'] ?? 'joke';
 
-//action replace ifelse
-$action = $_GET['action'] ?? 'home';
-
-//upperCase to lowerCase
-if($action == strtolower($action)) {
-    $jokeController->$action();
-    } else {
-        http_response_code(301);
-        header('Location: index.php?action=' . strtolower($action));
-        exit;
-    }
-
-
-$page = $jokeController->$action();
-
-// if(isset($_GET['edit'])) {
-//     $page = $jokeController->edit();
-// }else if (isset($_GET['delete'])) {
-//     $page = $jokeController->delete();
-// }else if(isset($_GET['list'])) {
-//     $page = $jokeController->list();
-// }else {
-//     $page = $jokeController->home();
-// }
-$title = $page['title'];
-
-$variables = $page['variables'] ?? [];
-$output = loadTemplate($page['template'], $variables);
-
-// if (isset($page['variables'])) {
-//     extract($page['variables']);
-// }
-//$output = $page['output'];
-// ob_start();
-
-// include __DIR__ . '/../templates/' . $page['template'];
-// $output = ob_get_clean();
-
-}catch (PDOException $e) {
-    $title = 'Error';
-    $output = 'Database error: ' . $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine();
+if ($cotrollerName === 'joke') {
+    $controller = new JokeController($jokesTable, $authorsTable);
+} else if ($controllerName === 'author') {
+    $controller = new AuthorController($authorsTable);
 }
+
+        if($action == strtolower($action) && $controllerName == strtolower($controllerName)) {
+            $page = $controller->$action();
+        }else {
+            http_response_code(301);
+            header('Location: index.php?controller=' . strtolower($controllerName) . '&action=' . strtolower($action));
+        }
+            $title = $page['title'];
+
+            $variables = $page['variables'] ?? [];
+            $output = loadTemplate($page['template'], $variables);
+    }
+    catch (PDOException $e) {
+        $title = 'Error';
+        $output = 'Database error: ' . $e->getMessage() . ' in ' . $e->getFile() . ':' . $e->getLine();
+
+    }
 
 include __DIR__ . '/../templates/layout.html.php';
